cf18869a2e7f76e137e172b50238e9f9ef36ac46
Markus Wals <MWals@at.ibm.com>
2016-09-17 00:58:20 +0200

    ...

--- micropi/nodes/micropi/micropi.js
+++ micropi/nodes/micropi/micropi.js
@@ -15,6 +15,7 @@ module.exports = function(RED) {
         var node = this;
         
 		var filename = options.filename;
+        var msgOut;
     
 		var audioStream = function(audioData) {
 			msgOut.stream=audioData;
@@ -64,81 +65,6 @@ module.exports = function(RED) {
 			}
 			
 		});
-			/*
-            if(msg.payload == true) {
-                node.log("msg received");
-				if (msg.path) {
-					path = msg.path;
-				} else {
-					path = "/home/pi/audio/";
-				}
-                node.log("path set to: " + path);
-				if (msg.filename) {
-					filename = msg.filename
-				} else {
-					filename = "test.wav";
-				}
-                node.log("filename set to: " + filename);
-				
-				http://stackoverflow.com/questions/34364583/how-to-use-the-write-function-for-wav-module-in-nodejs
-				
-				mic.startCapture();
-				node.log("started recording");
-                this.send([ {payload:"recording"} , {payload:"recording"} ]);
-				node.status({fill:"red",shape:"dot",text:"recording"});
-				
-				mic.audioStream.on('data', function(data) {
-					
-                    node.log(data);
-					var writer = new wav.FileWriter(path + filename, {
-						channels: 2,
-						sampleRate: 44100,
-						bitDepth: 16
-					});
-                    
-                    node.log("file metadata set to: 2Ch 16Bit 44.1kHz");
-                    
-					writer.write(String(data));
-                    node.log("writing data");
-                    
-                    if (writer.closeFileStream || data.closeFileStream) {
-                        node.status({}); //remove status
-                        writer.end();
-                        writer = null;
-                        node.log("writer.end() called");
-                        node.send(msg);
-                        node.status({}); //remove status
-                        var msg1 = {payload:writer};
-                        var msg2 = {file:path + filename};
-                        node.log("sending response message");
-                        node.send([ msg1 , msg2 ]);
-                    }
-				});				
-				
-				this.on('error', function(err) {
-					node.error('Error in microPi');
-					writer.end
-                    writer = null;
-					node.status({}); //remove status
-					msg1 = {payload:"error!"};
-				});
-				
-            } else if (msg.payload == false) {
-				mic.stopCapture();
-                node.log("stopped recording");
-                
-                //writer.end
-                //writer = undefined;
-				node.status({}); //remove status
-				//file.end();
-				
-				var msg1 = {payload:"stopped"};
-				var msg2 = {file:path + filename};
-				
-				node.log("sending response message");
-				this.send([ msg1 , msg2 ]);
-			}
-        });*/
     }
     RED.nodes.registerType('microPi', micropiNode);
 }
